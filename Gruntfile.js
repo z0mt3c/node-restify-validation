95ea14d1be1b3118b09164c8bca73471cfffe802
Timo Behrmann <timo.behrmann@gmail.com>
2013-07-11 19:49:37 +0200

    Travis

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,34 @@
-module.exports = function(grunt) {
-  // load all grunt tasks
-  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+module.exports = function (grunt) {
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-  // Project configuration.
-  grunt.initConfig({
-    mochacli: {
-      all: ['test/{,*/}*.js']
-    },
-    watch: {
-      scripts: {
-        files: ['<%= jshint.all %>'],
-        tasks: ['default']
-      }
-    },
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      all: [
-        'Gruntfile.js',
-        'lib/*.js',
-        'test/*.js'
-      ]
-    }
-  });
+    // Project configuration.
+    grunt.initConfig({
+        mochacli: {
+            options: {
+                reporter: 'spec'
+            },
+            all: ['test/{,*/}*.js']
+        },
+        watch: {
+            scripts: {
+                files: ['<%= jshint.all %>'],
+                tasks: ['default']
+            }
+        },
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            all: [
+                'Gruntfile.js',
+                'lib/*.js',
+                'test/*.js'
+            ]
+        }
+    });
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'mochacli']);
+    // Default task.
+    grunt.registerTask('test', ['jshint', 'mochacli']);
+    grunt.registerTask('default', ['test']);
 };
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 node-restify-validation
 =======================
-
 Validation for REST Services built with node-restify in node.js
+
+[![Build Status](https://travis-ci.org/z0mt3c/node-restify-validation.png)](https://travis-ci.org/z0mt3c/node-restify-validation)
+
