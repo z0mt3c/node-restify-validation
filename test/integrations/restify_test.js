76a8649551bb71930cd1f5716070eab11209d2cc
Stefan Schärmeli <schaermu@gmail.com>
2016-10-04 12:13:59 +0200

    cleaned up indention

--- test/integrations/restify_test.js
+++ test/integrations/restify_test.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * The MIT License
  *
  * Copyright 2015 Timo Behrmann, Guillaume Chauvet
@@ -27,125 +27,146 @@ var restify = require('restify');
 var validationParser = require('../../lib').validationPlugin;
 var request = require('supertest');
 
-describe("[INTEGRATION][RESTIFY]", function () {
+describe("[INTEGRATION][RESTIFY]", function() {
 
-    describe('ISSUES', function() {
-      
-      describe('#32', function() {
-  
-	var server;
-	
-	before(function (done) {
-	    server = restify.createServer();
-	    server.use(restify.bodyParser({mapParams: false}));
-	    server.use(validationParser({
-	      forbidUndefinedVariables: true
-	    }));
-	    server.listen(0, function() {
-		server.post({
-		  url: '/foo/:id',
-		  validation: {
-		    resources: {
-			id: { isRequired: true, 'swaggerScope': 'path' }
-		    },
-		    content: {
-			name: { isRequired: false },
-			grades: { isRequired: false },
-			school: { isRequired: false },
-			subjectArea: { isRequired: false }
-		    }
-		  }
-		}, function (req, res, next) {
-		    res.send(req.body);
-		    next();
-		});
-		done();
-	    });
-	});
+  describe('ISSUES', function() {
 
-	after(function (done) {
-	    server.close(done);
-	});
+    describe('#32', function() {
 
-      
-	describe("on GET", function() {
-	
-	    it("not allowed route", function (done) {
-		request(server)
-		.get('/foo')
-		.expect(404)
-		.end(done);
-	    });
+      var server;
 
-	});
-	
-	describe("on POST", function() {
-	
-	    it("with 1 optional field", function (done) {
-		request(server)
-		.post('/foo/73')
-		.send({"name": "test"})
-		.expect(200, {"name": "test"})
-		.end(done);
-	    });
+      before(function(done) {
+        server = restify.createServer();
+        server.use(restify.bodyParser({
+          mapParams: false
+        }));
+        server.use(validationParser({
+          forbidUndefinedVariables: true
+        }));
+        server.listen(0, function() {
+          server.post({
+            url: '/foo/:id',
+            validation: {
+              resources: {
+                id: {
+                  isRequired: true,
+                  'swaggerScope': 'path'
+                }
+              },
+              content: {
+                name: {
+                  isRequired: false
+                },
+                grades: {
+                  isRequired: false
+                },
+                school: {
+                  isRequired: false
+                },
+                subjectArea: {
+                  isRequired: false
+                }
+              }
+            }
+          }, function(req, res, next) {
+            res.send(req.body);
+            next();
+          });
+          done();
+        });
+      });
 
-	});
+      after(function(done) {
+        server.close(done);
+      });
+
+
+      describe("on GET", function() {
+
+        it("not allowed route", function(done) {
+          request(server)
+            .get('/foo')
+            .expect(404)
+            .end(done);
+        });
 
       });
-      
-      describe('#37', function() {
-  
-	var server;
-	
-	before(function (done) {
-	    server = restify.createServer();
-	    server.use(restify.bodyParser({mapParams: false}));
-	    server.use(validationParser({
-	      mapParams: true
-	    }));
-	    server.use(restify.queryParser({mapParams: false}));
-	    server.listen(0, function() {
-		server.get({
-		  url: '/test/:name',
-		  validation: {
-		    queries: {
-		      age: {
-			isRequired: true
-		      }
-		    }
-		  }
-		}, function (req, res, next) {
-		    res.send(req.params);
-		    next();
-		});
-		done();
-	    });
-	});
 
-	after(function (done) {
-	    server.close(done);
-	});
+      describe("on POST", function() {
 
-      
-	describe("on GET", function() {
-	
-	    it("no route", function (done) {
-		request(server)
-		.get('/test')
-		.expect(404)
-		.end(done);
-	    });
-	    
-	    it("with undefined query", function (done) {
-		request(server)
-		.get('/test/foo')
-		.expect(400)
-		.end(done);
-	    });
-
-	});
+        it("with 1 optional field", function(done) {
+          request(server)
+            .post('/foo/73')
+            .send({
+              "name": "test"
+            })
+            .expect(200, {
+              "name": "test"
+            })
+            .end(done);
+        });
 
       });
 
     });
+
+    describe('#37', function() {
+
+      var server;
+
+      before(function(done) {
+        server = restify.createServer();
+        server.use(restify.bodyParser({
+          mapParams: false
+        }));
+        server.use(validationParser({
+          mapParams: true
+        }));
+        server.use(restify.queryParser({
+          mapParams: false
+        }));
+        server.listen(0, function() {
+          server.get({
+            url: '/test/:name',
+            validation: {
+              queries: {
+                age: {
+                  isRequired: true
+                }
+              }
+            }
+          }, function(req, res, next) {
+            res.send(req.params);
+            next();
+          });
+          done();
+        });
+      });
+
+      after(function(done) {
+        server.close(done);
+      });
+
+
+      describe("on GET", function() {
+
+        it("no route", function(done) {
+          request(server)
+            .get('/test')
+            .expect(404)
+            .end(done);
+        });
+
+        it("with undefined query", function(done) {
+          request(server)
+            .get('/test/foo')
+            .expect(400)
+            .end(done);
+        });
+
+      });
+
+    });
+
+  });
 });
